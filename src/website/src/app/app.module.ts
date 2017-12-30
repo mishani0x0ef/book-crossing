256060f0110f71fc0c1bd0b0b5db2400788e86e3
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2017-12-31 01:33:52 +0200

    Add material components.

--- src/website/src/app/app.module.ts
+++ src/website/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+
+import { MaterialModule } from './material.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
